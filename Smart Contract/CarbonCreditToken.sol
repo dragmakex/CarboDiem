c7b94fe9bb8c62f871eaedd4c0841f0ddf767eae
abcdam <lukasada@students.zhaw.ch>
2023-10-22 11:25:23 +0200

    Unfinished collection of smart contracts for funding campaigns, carbon credit tokens, and escrow service

---
+++ Smart Contract/CarbonCreditToken.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity ^0.8.20;
+ 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CCToken is ERC20, Ownable {
+    uint8 public weightPercentage;
+
+    event WeightAdjustment(address indexed weightAdjuster, uint8 percentage);
+
+    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol){ 
+        weightPercentage = 100;
+        _mint(msg.sender, initialSupply*(10**uint256(decimals())));
+    }
+
+    function retire(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount);
+        _burn(msg.sender, amount);
+    }
+
+    function adjustWeight(uint8 percent) public onlyOwner{
+        require(percent >= 0 && percent <=100, "percentage out of range");
+        weightPercentage = percent;
+        emit WeightAdjustment(msg.sender, percent)
+    }
+}
---
+++ Smart Contract/EscrowContract.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../contracts/CarbonCreditToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EscrowContract is Ownable {
+    struct LockedCredit {
+        CarbonCreditToken token;
+        uint256 emissionRate;
+    }
+    struct LockedStablecoin {
+        IERC20 token;
+        uint256 emissionRate;
+    }
+
+    LockedCredit public carbonCredits;
+    LockedStablecoin public stablecoins;
+
+    address public creditProducer;
+    address public creditRecipient;
+
+    uint256 public maturationTime;  // number of seconds until all tokens emitted
+    uint256 public maturationDate;  // future point in time where contract is fulfilled
+    uint256 public emissionRate;    // number of tokens released per second
+
+
+    event TokensDeposited(uint256 amountA, uint256 amountB);
+    event TokensReleased(uint256 amountA, uint256 amountB);
+    event DestroyContract(address indexed _from, uint256 amountStables);
+
+    constructor(
+        address _CCtoken,
+        address _stablecoin,
+        address _payeeCCproducer,
+        address _payeeCCconsumer,
+        uint256 _maturationTime
+    ) {
+        carbonCredits = LockedCredit({
+            token : CarbonCreditToken(_CCtoken),
+            emissionRate : 0
+        });
+        stablecoins = LockedStablecoin({
+            token : IERC20(_stablecoin),
+            emissionRate : 0
+        });
+        creditProducer  = _payeeCCproducer;
+        creditRecipient = _payeeCCconsumer;
+        maturationTime  = _maturationTime;
+    }
+
+    function depositTokens(uint256 amountCarbonCredits, uint256 amountStablecoin) public payable onlyOwner {
+        require(maturationTime == 0, "Tokens already deposited.");
+        require(carbonCredits.token.transferFrom(msg.sender, address(this), amountCarbonCredits), "Transfer of carbon credits failed.");
+        require(stablecoins.token.transferFrom(msg.sender, address(this), amountStablecoin), "Transfer of stablecoin failed.");
+        maturationDate = block.timestamp + maturationTime;
+        carbonCredits.emissionRate = amountCarbonCredits / maturationTime
+        stablecoins.emissionRate = amountStablecoin / maturationTime;
+        emit TokensDeposited(amountCarbonCredits, amountStablecoin);
+    }
+
+    function releaseTokens() external {
+        require(maturationTime > 0, "All tokens already emitted");
+        uint256 timePassed;
+        if(maturationDate - block.timestamp > 0) {
+            timePassed = maturationTime - (maturationDate - block.timestamp); // time passed since contract inception or last withdrawal
+        }
+        else{
+            timePassed = maturationTime
+        }
+        uint256 claimedCarbonCredits = timePassed * carbonCredits.emissionRate;
+        uint256 claimedStablecoins  = timePassed * stablecoins.emissionRate;
+        maturationTime -= timePassed;
+        require(carbonCredits.token.transfer(creditRecipient, claimedCarbonCredits), "Carbon credits transfer failed.");
+        require(stablecoins.token.transfer(creditProducer, claimedStablecoins), "Stablecoin transfer failed.");
+        emit TokensReleased(claimedCarbonCredits, claimedStablecoins);
+    }
+
+    function revertContract() external onlyOwner {
+        carbonCredits.token._burn(address(this),carbonCredits.token.balanceOf(address(this)));
+        uint256 remainingBalance = stablecoins.token.balanceOf(address(this));
+        stablecoins.token.transfer(creditRecipient,remainingBalance);
+        renounceOwnership();
+        emit(msg.sender, remainingBalance);
+    }
+}
